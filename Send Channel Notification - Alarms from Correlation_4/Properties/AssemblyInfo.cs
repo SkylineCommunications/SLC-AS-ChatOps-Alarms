8a1c60ee78406fa50625a9ba0411bb21fa3cfb45
Bruno Dias <bruno.dias@skyline.be>
2023-04-20 17:24:48 +0100

    Initial commit.

---
+++ Send Channel Notification - Alarms from Correlation_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Send Channel Notification - Alarms from Correlation_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Send Channel Notification - Alarms from Correlation_4")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A55CF0F0-0118-491F-BCE3-20C9107D93EF")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Send Channel Notification - Alarms from Correlation_4/Send Channel Notification - Alarms from Correlation_4.cs
@@ -0,0 +1,84 @@
+using System;
+using Skyline.DataMiner.Automation;
+using Skyline.DataMiner.DcpChatIntegrationHelper.Common;
+using Skyline.DataMiner.DcpChatIntegrationHelper.Teams;
+using Skyline.DataMiner.Library.Automation;
+using Skyline.DataMiner.Library.Common;
+
+public class Script
+{
+	public void Run(Engine engine)
+	{
+		var chatIntegrationHelper = new ChatIntegrationHelperBuilder().Build();
+		try
+		{
+			var teamIdParam = engine.GetScriptParam("Team Id");
+			if (string.IsNullOrWhiteSpace(teamIdParam?.Value))
+			{
+				engine.ExitFail("'Team Id' parameter is required.");
+				return;
+			}
+
+			var channelIdParam = engine.GetScriptParam("Channel Id");
+			if (string.IsNullOrWhiteSpace(channelIdParam?.Value))
+			{
+				engine.ExitFail("'Channel Id' parameter is required.");
+				return;
+			}
+
+			var dms = engine.GetDms();
+
+			engine.GenerateInformation("Info: " + engine.GetScriptParam(65006).Value);
+			string[] alarmInfo = engine.GetScriptParam(65006).Value.Split('|');
+
+			string alarmId = alarmInfo[0];
+			int dmaId = Convert.ToInt32(alarmInfo[1]);
+			int elementId = Convert.ToInt32(alarmInfo[2]);
+			string paramId = alarmInfo[3];
+			//string paramIdx = alarmInfo[4];
+			AlarmLevel severity = (AlarmLevel)Convert.ToInt32(alarmInfo[7]);
+			string status = alarmInfo[9];
+			string value = alarmInfo[10];
+
+			// string elementName = engine.FindElement(dmaId, elementId).ElementName;
+			var element = dms.GetElement(new Skyline.DataMiner.Library.Common.DmsElementId(dmaId, elementId));
+			string elementName = element.Name;
+			// string parameterDescr = tbd
+
+			engine.GenerateInformation("severity: " + severity);
+			engine.GenerateInformation("elementName: " + elementName);
+			
+			try
+			{
+				// chatIntegrationHelper.Teams.TrySendChannelNotification(teamIdParam.Value, channelIdParam.Value, engine.GetScriptParam(65006).Value);
+
+				chatIntegrationHelper.Teams.TrySendChannelNotification(teamIdParam.Value, channelIdParam.Value,
+					$"New Alarm - Alarm ID: {alarmId}; Severity: {severity}; Element: {elementName}; Parameter: {paramId}; Value: {value}; Status: {status};");
+
+				// chatIntegrationHelper.Teams.TrySendChannelNotification(teamIdParam.Value, channelIdParam.Value, $" *** New Alarm *** Alarm ID: {alarmId} Element: {elementName} Severity: {severity}");
+
+				// chatIntegrationHelper.Teams.TrySendChannelNotification(teamIdParam.Value, channelIdParam.Value, notificationParam.Value);
+			}
+			catch (TeamsChatIntegrationException e)
+			{
+				engine.ExitFail($"Couldn't send the notification to the channel with id {channelIdParam.Value} with error {e.Message}.");
+				return;
+			}
+
+			engine.ExitSuccess($"The notification was sent to the channel with id {channelIdParam.Value}!");
+		}
+		catch (ScriptAbortException)
+		{
+			// Also ExitSuccess is a ScriptAbortException
+			throw;
+		}
+		catch (Exception e)
+		{
+			engine.ExitFail($"An exception occurred with the following message: {e.Message}");
+		}
+		finally
+		{
+			chatIntegrationHelper?.Dispose();
+		}
+	}
+}
